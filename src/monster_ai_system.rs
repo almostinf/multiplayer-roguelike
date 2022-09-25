17060379072e2a9e2872747fdaef8fe652eaff4e
almostinf <almostinf@mail.ru>
2022-09-25 13:10:23 +0300

    added monster chase

---
+++ src/monster_ai_system.rs
@@ -0,0 +1,50 @@
+use specs::prelude::*;
+use crate::{xy_idx, RunState, WantsToMelee};
+
+use super::{Viewshed, Monster, Name, Map, Position};
+use rltk::{Point, console};
+
+pub struct MonsterAI {}
+
+impl<'a> System<'a> for MonsterAI {
+    #[allow(clippy::type_complexity)]
+    type SystemData = ( WriteExpect<'a, Map>,
+                        ReadExpect<'a, Point>,
+                        ReadExpect<'a, Entity>,
+                        ReadExpect<'a, RunState>,
+                        Entities<'a>,
+                        WriteStorage<'a, Viewshed>,
+                        ReadStorage<'a, Monster>,
+                        WriteStorage<'a, Position>,
+                        WriteStorage<'a, WantsToMelee>);
+    
+    fn run(&mut self, data : Self::SystemData) {
+        let (mut map, player_pos, player_entity, runstate, entities, mut viewshed, monster, mut position, mut wants_to_melee) = data;
+
+        if *runstate != RunState::MonsterTurn {
+            return;
+        }
+        for (entity, mut viewshed, _monster, mut pos) in (&entities, &mut viewshed, &monster, &mut position).join() {
+            let distance = rltk::DistanceAlg::Pythagoras.distance2d(Point::new(pos.x, pos.y), *player_pos);
+            if distance < 1.5 {
+                wants_to_melee.insert(entity, WantsToMelee { target: *player_entity }).expect("Unable to insert attack");
+            } else if viewshed.visible_tiles.contains(&*player_pos) {
+                // Path to the player
+                let path = rltk::a_star_search(
+                    xy_idx(pos.x, pos.y),
+                    xy_idx(player_pos.x, player_pos.y),
+                    &mut *map
+                );
+                if path.success && path.steps.len() > 1 {
+                    let mut idx = xy_idx(pos.x, pos.y);
+                    map.blocked[idx] = false;
+                    pos.x = path.steps[1] as i32 % map.width;
+                    pos.y = path.steps[1] as i32 / map.width;
+                    idx = xy_idx(pos.x, pos.y);
+                    map.blocked[idx] = true;
+                    viewshed.dirty = true;
+                }
+            }
+        }
+    }
+}
