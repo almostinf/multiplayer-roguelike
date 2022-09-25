d254cc32994608fa9cac0f31be4964b91779e27d
almostinf <almostinf@mail.ru>
2022-09-25 13:16:13 +0300

    expended movements of player and added ability to make damage

--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,7 @@
-use rltk::{VirtualKeyCode, Rltk};
+use rltk::{VirtualKeyCode, Rltk, Point, console};
 use specs::prelude::*;
+use crate::{RunState, CombatStats, WantsToMelee};
+
 use super::{Position, Player, TileType, State, Viewshed, Map};
 use super::{xy_idx};
 use std::cmp::{min, max};
@@ -8,22 +10,43 @@ pub fn try_move_player(delta_x : i32, delta_y : i32, ecs : &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
     let mut viewsheds = ecs.write_storage::<Viewshed>();
+    let combat_stats = ecs.read_storage::<CombatStats>();
     let map = ecs.fetch::<Map>();
+    let entities = ecs.entities();
+    let mut wants_to_melee = ecs.write_storage::<WantsToMelee>();
 
-    for (_player, pos, viewshed) in (&mut players, &mut positions, &mut viewsheds).join() {
+    for (entity, _player, pos, viewshed) in (&entities, &mut players, &mut positions, &mut viewsheds).join() {
+        if pos.x + delta_x < 1 || pos.x + delta_x > map.width-1 || pos.y + delta_y < 1 || pos.y + delta_y > map.height-1 { return; }
         let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
-        if map.tiles[destination_idx] != TileType::Wall {
+
+        for potential_target in map.tile_content[destination_idx].iter() {
+            let target = combat_stats.get(*potential_target);
+            if let Some(_target) = target {
+                wants_to_melee.insert(entity, WantsToMelee { 
+                    target: *potential_target 
+                })
+                .expect("Add target failed");
+            }
+        }
+
+        if !map.blocked[destination_idx] {
             pos.x = min(79, max(0, pos.x + delta_x));
             pos.y = min(49, max(0, pos.y + delta_y));
+
+            let mut ppos = ecs.write_resource::<Point>();
+            ppos.x = pos.x;
+            ppos.y = pos.y;
 
             viewshed.dirty = true;
         }
     }
 }
 
-pub fn player_input(gs : &mut State, ctx : &mut Rltk) {
+pub fn player_input(gs : &mut State, ctx : &mut Rltk) -> RunState {
     match ctx.key {
-        None => {},
+        None => {
+            return RunState::AwaitingInput
+        },
         Some(key) => match key {
             VirtualKeyCode::Left |
             VirtualKeyCode::A => try_move_player(-1, 0, &mut gs.ecs),
@@ -33,7 +56,20 @@ pub fn player_input(gs : &mut State, ctx : &mut Rltk) {
             VirtualKeyCode::W => try_move_player(0, -1, &mut gs.ecs),
             VirtualKeyCode::Down |
             VirtualKeyCode::S => try_move_player(0, 1, &mut gs.ecs),
-            _ => {}
+
+            // Diagonals
+            VirtualKeyCode::Numpad9 | 
+            VirtualKeyCode::E => try_move_player(1, -1, &mut gs.ecs),
+            VirtualKeyCode::Numpad7 |
+            VirtualKeyCode::Q => try_move_player(-1, -1, &mut gs.ecs),
+            VirtualKeyCode::Numpad3 |
+            VirtualKeyCode::C => try_move_player(1, 1, &mut gs.ecs),
+            VirtualKeyCode::Numpad1 |
+            VirtualKeyCode::Z => try_move_player(-1, 1, &mut gs.ecs),
+            _ => {
+                return RunState::AwaitingInput
+            }
         }
     }
+    RunState::PlayerTurn
 }
