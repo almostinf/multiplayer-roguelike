8ee9bb91695924b86c71569c446b6f5f09d9d2b8
almostinf <almostinf@mail.ru>
2022-10-01 11:42:38 +0300

    added ability to take,  drop and view items

--- src/player.rs
+++ src/player.rs
@@ -1,8 +1,8 @@
 use rltk::{VirtualKeyCode, Rltk, Point, console};
 use specs::prelude::*;
-use crate::{RunState, CombatStats, WantsToMelee};
+use crate::{RunState, CombatStats, WantsToMelee, gamelog, GameLog};
 
-use super::{Position, Player, TileType, State, Viewshed, Map};
+use super::{Position, Player, TileType, State, Viewshed, Map, Item, WantsToPickupItem};
 use super::{xy_idx};
 use std::cmp::{min, max};
 
@@ -42,6 +42,31 @@ pub fn try_move_player(delta_x : i32, delta_y : i32, ecs : &mut World) {
     }
 }
 
+fn get_item(ecs: &mut World) {
+    let player_pos = ecs.fetch::<Point>();
+    let player_entity = ecs.fetch::<Entity>();
+    let entities = ecs.entities();
+    let items = ecs.read_storage::<Item>();
+    let positions = ecs.read_storage::<Position>();
+    let mut gamelog = ecs.fetch_mut::<GameLog>();
+
+    let mut target_item : Option<Entity> = None;
+    for (item_entity, _item, position) in (&entities, &items, &positions).join() {
+        if position.x == player_pos.x && position.y == player_pos.y {
+            target_item = Some(item_entity);
+        }
+    }
+
+    match target_item {
+        None => gamelog.entries.push("There is nothing here to pick up.".to_string()),
+        Some(item) => {
+            let mut pickup = ecs.write_storage::<WantsToPickupItem>();
+            pickup.insert(*player_entity, WantsToPickupItem { collected_by: *player_entity, item}).expect("Unable to insert want to pickup");
+        }
+    }
+
+}
+
 pub fn player_input(gs : &mut State, ctx : &mut Rltk) -> RunState {
     match ctx.key {
         None => {
@@ -66,6 +91,9 @@ pub fn player_input(gs : &mut State, ctx : &mut Rltk) -> RunState {
             VirtualKeyCode::C => try_move_player(1, 1, &mut gs.ecs),
             VirtualKeyCode::Numpad1 |
             VirtualKeyCode::Z => try_move_player(-1, 1, &mut gs.ecs),
+            VirtualKeyCode::G => get_item(&mut gs.ecs),
+            VirtualKeyCode::I => return RunState::ShowInventory,
+            VirtualKeyCode::D => return RunState::ShowDropItem,
             _ => {
                 return RunState::AwaitingInput
             }
