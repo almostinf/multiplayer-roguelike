fe1c20770b29644cb3a72b4d45eb056d6a5ede81
almostinf <almostinf@mail.ru>
2022-10-04 22:14:55 +0300

    improve working with items in spawner.rs

--- src/spawner.rs
+++ src/spawner.rs
@@ -2,7 +2,9 @@ use std::process::id;
 
 use rltk::{RGB, RandomNumberGenerator};
 use specs::prelude::*;
-use super::{CombatStats, Player, Renderable, Name, Position, Viewshed, Monster, BlocksTile, Rect, Item, Potion};
+use crate::AreaOfEffect;
+
+use super::{CombatStats, Player, Renderable, Name, Position, Viewshed, Monster, BlocksTile, Rect, Item, ProvidesHealing, Consumable, Ranged, InflictDamage, Confusion};
 use super::{MAPCOUNT, MAPHEIGHT, MAPWIDTH};
 
 const MAX_MONSTERS : i32 = 4;
@@ -112,7 +114,7 @@ pub fn spawn_room(ecs: &mut World, room: &Rect) {
     for idx in item_spawn_points.iter() {
         let x = *idx % MAPWIDTH;
         let y = *idx / MAPWIDTH;
-        health_potion(ecs, x as i32, y as i32);
+        random_item(ecs, x as i32, y as i32);
     }
 }
 
@@ -127,6 +129,73 @@ fn health_potion(ecs: &mut World, x : i32, y: i32) {
         })
         .with(Name {name: "Health Potion".to_string()})
         .with(Item {})
-        .with(Potion {heal_amount: 8})
+        .with(ProvidesHealing {heal_amount: 8})
+        .with(Consumable {})
+        .build();
+}
+
+fn magic_missible_scroll(ecs : &mut World, x : i32, y : i32) {
+    ecs.create_entity()
+        .with(Position {x, y})
+        .with(Renderable {
+            glyph : rltk::to_cp437(')'),
+            fg : RGB::named(rltk::CYAN),
+            bg : RGB::named(rltk::BLACK),
+            render_order : 2,
+        })
+        .with(Name {name : "Magic Missible Scroll".to_string()})
+        .with(Item {})
+        .with(Consumable {})
+        .with(Ranged {range : 6})
+        .with(InflictDamage {damage : 8})
+        .build();
+}
+
+fn random_item(ecs : &mut World, x : i32, y : i32) {
+    let roll : i32;
+    {
+        let mut rng = ecs.write_resource::<RandomNumberGenerator>();
+        roll = rng.roll_dice(1, 4);
+    }
+    match roll {
+        1 => health_potion(ecs, x, y),
+        2 => fireball_scroll(ecs, x, y),
+        3 => confusion_scroll(ecs, x, y),
+        _ => magic_missible_scroll(ecs, x, y)
+    }
+}
+
+fn fireball_scroll(ecs : &mut World, x : i32, y : i32) {
+    ecs.create_entity()
+        .with(Position {x, y})
+        .with(Renderable {
+            glyph : rltk::to_cp437(')'),
+            fg : RGB::named(rltk::ORANGE),
+            bg : RGB::named(rltk::BLACK),
+            render_order : 2,
+        })
+        .with(Name {name : "Fireball Scroll".to_string()})
+        .with(Item {})
+        .with(Consumable {})
+        .with(Ranged {range : 6})
+        .with(InflictDamage {damage : 20})
+        .with(AreaOfEffect {radius : 3})
+        .build();
+}
+
+fn confusion_scroll(ecs : &mut World, x : i32, y : i32) {
+    ecs.create_entity()
+        .with(Position {x, y})
+        .with(Renderable {
+            glyph : rltk::to_cp437(')'),
+            fg : RGB::named(rltk::PINK),
+            bg : RGB::named(rltk::BLACK),
+            render_order : 2,
+        })
+        .with(Name {name : "Confusion Scroll".to_string()})
+        .with(Item {})
+        .with(Consumable {})
+        .with(Ranged {range : 6})
+        .with(Confusion {turns : 4})
         .build();
 }
