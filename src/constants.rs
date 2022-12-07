92d8d65ff249ae7a58cf3d15cccdb8336b887da6
almostinf <almostinf@mail.ru>
2022-12-07 17:18:48 +0300

    put all constants in one file

---
+++ src/constants.rs
@@ -0,0 +1,9 @@
+// This file is needed to store the constants in one place
+
+pub const MAX_ROOMS : i32 = 30;
+pub const MIN_SIZE : i32 = 6;
+pub const MAX_SIZE : i32 = 10;
+pub const MAPWIDTH : usize = 80;
+pub const MAPHEIGHT : usize = 43;
+pub const MAPCOUNT : usize = MAPHEIGHT * MAPWIDTH;
+pub const MAX_MONSTERS : i32 = 4;
